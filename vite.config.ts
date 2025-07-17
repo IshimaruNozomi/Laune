25ddaf952c8aeacb2dc5350953d680630ef0d79d
Nozomi Ishimaru <s2322096@stu.musashino-u.ac.jp>
2025-07-17 23:20:26 +0900

    もっかい変更

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Laune/',
+  base: process.env.NODE_ENV === 'production' ? '/Laune/' : '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
